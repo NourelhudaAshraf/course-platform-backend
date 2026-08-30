c2821b8c53817b0008ecf0f2c47ea13f5d3c9235
Nourelhoda Ashraf <Nourelhoda.Ashraf@linkdev.com>
2026-08-30 17:44:54 +0300

    refactor enrollment module

--- controllers/enrollment.controller.js
+++ controllers/enrollment.controller.js
@@ -2,8 +2,6 @@ const env = require("../config/env");
 const Stripe = require("stripe");
 const stripe = new Stripe(env.STRIPE_SECRET_KEY);
 const Enrollment = require("../models/enrollment.model");
-const UserLesson = require("../models/user-lesson.model");
-const Lesson = require("../models/lesson.model");
 const catchAsync = require("../utils/catch-async");
 const { getAllDocs } = require("../utils/handle-factory");
 const {
@@ -53,142 +51,12 @@ const checkIfCourseEnrolled = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: "success", data: enrolled });
 });
 
-const getEnrolledCourses = catchAsync(async (req, res, next) => {
+const getEnrolledCourses = catchAsync(async (req, res) => {
   const userId = req.user._id;
-
-  const page = Number(req.query.page) || 1;
-  const limit = Math.min(Number(req.query.limit) || 10, 25);
-  const skip = (page - 1) * limit;
-  const totalItems = await Enrollment.countDocuments();
-  const totalPages = Math.ceil(totalItems / limit);
-
-  const pipeline = [
-    // 1. match user's enrollments
-    {
-      $match: {
-        user: userId,
-      },
-    },
-    {
-      $lookup: {
-        from: "courses",
-        localField: "course",
-        foreignField: "_id",
-        as: "course",
-      },
-    },
-    // 3. Convert course array → object as lookup return as array
-    {
-      $unwind: "$course",
-    },
-    // 4. Get all lessons belonging to this course
-    {
-      $lookup: {
-        from: "lessons",
-        //let -> creates a variable inside the $lookup.
-        let: { courseId: "$course._id" },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                // Lesson.course === Enrollment.course
-                $eq: ["$course", "$$courseId"],
-              },
-            },
-          },
-          {
-            //I only need its ID.
-            $project: {
-              _id: 1,
-            },
-          },
-        ],
-        as: "lessons",
-      },
-    },
-    // 5. Get lessons watched by user
-    {
-      $lookup: {
-        from: "userlessons",
-        let: {
-          lessonIds: "$lessons._id",
-        },
-        pipeline: [
-          {
-            $match: {
-              user: userId,
-              completed: true,
-              $expr: {
-                $in: ["$lesson", "$$lessonIds"],
-              },
-            },
-          },
-        ],
-        as: "completedLessons",
-      },
-    },
-    // 6. add progress object
-    {
-      $addFields: {
-        progress: {
-          totalLessons: {
-            $size: "$lessons",
-          },
-          completedLessons: {
-            $size: "$completedLessons",
-          },
-          percentage: {
-            $cond: [
-              { $eq: [{ $size: "$lessons" }, 0] },
-              0,
-              {
-                $round: [
-                  {
-                    $multiply: [
-                      {
-                        $divide: [
-                          { $size: "$completedLessons" },
-                          { $size: "$lessons" },
-                        ],
-                      },
-                      100,
-                    ],
-                  },
-                  0,
-                ],
-              },
-            ],
-          },
-        },
-      },
-    },
-    // 7. Remove temporary arrays
-    {
-      $project: {
-        lessons: 0,
-        completedLessons: 0,
-      },
-    },
-    // 8. Pagination
-    {
-      $sort: {
-        createdAt: -1,
-      },
-    },
-    {
-      $skip: skip,
-    },
-    {
-      $limit: limit,
-    },
-  ];
-  const docs = await Enrollment.aggregate(pipeline);
+  const result = await enrolledCoursesService(userId);
   res.status(200).json({
     status: "success",
-    results: docs.length,
-    totalPages,
-    page,
-    data: docs,
+    ...result,
   });
 });
 
--- services/enrollment.service.js
+++ services/enrollment.service.js
@@ -176,8 +176,145 @@ const checkIfCourseEnrolledService = async (user, course) => {
   return !!enroll;
 };
 
+const enrolledCoursesService = async (userId) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Math.min(Number(req.query.limit) || 10, 25);
+  const skip = (page - 1) * limit;
+  const totalItems = await Enrollment.countDocuments();
+  const totalPages = Math.ceil(totalItems / limit);
+
+  const pipeline = [
+    // 1. match user's enrollments
+    {
+      $match: {
+        user: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "courses",
+        localField: "course",
+        foreignField: "_id",
+        as: "course",
+      },
+    },
+    // 3. Convert course array → object as lookup return as array
+    {
+      $unwind: "$course",
+    },
+    // 4. Get all lessons belonging to this course
+    {
+      $lookup: {
+        from: "lessons",
+        //let -> creates a variable inside the $lookup.
+        let: { courseId: "$course._id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                // Lesson.course === Enrollment.course
+                $eq: ["$course", "$$courseId"],
+              },
+            },
+          },
+          {
+            //I only need its ID.
+            $project: {
+              _id: 1,
+            },
+          },
+        ],
+        as: "lessons",
+      },
+    },
+    // 5. Get lessons watched by user
+    {
+      $lookup: {
+        from: "userlessons",
+        let: {
+          lessonIds: "$lessons._id",
+        },
+        pipeline: [
+          {
+            $match: {
+              user: userId,
+              completed: true,
+              $expr: {
+                $in: ["$lesson", "$$lessonIds"],
+              },
+            },
+          },
+        ],
+        as: "completedLessons",
+      },
+    },
+    // 6. add progress object
+    {
+      $addFields: {
+        progress: {
+          totalLessons: {
+            $size: "$lessons",
+          },
+          completedLessons: {
+            $size: "$completedLessons",
+          },
+          percentage: {
+            $cond: [
+              { $eq: [{ $size: "$lessons" }, 0] },
+              0,
+              {
+                $round: [
+                  {
+                    $multiply: [
+                      {
+                        $divide: [
+                          { $size: "$completedLessons" },
+                          { $size: "$lessons" },
+                        ],
+                      },
+                      100,
+                    ],
+                  },
+                  0,
+                ],
+              },
+            ],
+          },
+        },
+      },
+    },
+    // 7. Remove temporary arrays
+    {
+      $project: {
+        lessons: 0,
+        completedLessons: 0,
+      },
+    },
+    // 8. Pagination
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+  ];
+  const docs = await Enrollment.aggregate(pipeline);
+  return {
+    results: docs.length,
+    totalPages,
+    page,
+    data: docs,
+  };
+};
+
 module.exports = {
   getCheckoutSessionService,
   webhookHandlerService,
   checkIfCourseEnrolledService,
+  enrolledCoursesService,
 };
